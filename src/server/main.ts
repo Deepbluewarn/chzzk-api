33b10776ff507d4747b4b0a957ee3d08262595cb
bluewarn <n5lp97@gmail.com>
2024-12-21 07:33:44 +0000

    feat: soop 플랫폼 배지 목록 추가

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,6 +2,8 @@ import express from "express";
 import ViteExpress from "vite-express";
 import { morganMiddleware } from "./logger/morganMiddleware.js";
 import cors from 'cors';
+import chzzkBadgeList from "./chzzk.js";
+import soopBadgeList from "./soop.js";
 
 const app = express();
 const allowedOrigin = [
@@ -13,64 +15,15 @@ app.use(express.json());
 app.use(morganMiddleware);
 
 app.post('/badges', (req, res) => {
+  res.json(chzzkBadgeList);
+});
 
-  res.json([
-    {
-      id: 'streamer',
-      name: '스트리머',
-      image: 'https://ssl.pstatic.net/static/nng/glive/icon/streamer.png',
-    },
-    {
-      id: 'manager',
-      name: '매니저',
-      image: 'https://ssl.pstatic.net/static/nng/glive/icon/manager.png',
-    },
-    {
-      id: 'verified',
-      name: '인증 마크',
-      image: 'https://ssl.pstatic.net/static/nng/glive/resource/p/static/media/icon_official.a53d1555f8f4796d7862.png',
-    },
-    {
-      id: 'fan_1',
-      name: '후원 팬',
-      image: 'https://ssl.pstatic.net/static/nng/glive/icon/fan.png',
-    },
-    {
-      id: 'fan_2',
-      name: 'Gold',
-      image: 'https://ssl.pstatic.net/static/nng/glive/icon/gold.png',
-    },
-    {
-      id: 'fan_3',
-      name: 'Silver',
-      image: 'https://ssl.pstatic.net/static/nng/glive/icon/silver.png',
-    },
-    {
-      id: 'fan_4',
-      name: 'Bronze',
-      image: 'https://ssl.pstatic.net/static/nng/glive/icon/bronze.png',
-    },
-    {
-      id: 'cheese_1',
-      name: '치즈 1',
-      image: 'https://ssl.pstatic.net/static/nng/glive/icon/cheese01.png',
-    },
-    {
-      id: 'cheese_2',
-      name: '치즈 2',
-      image: 'https://ssl.pstatic.net/static/nng/glive/icon/cheese02.png',
-    },
-    {
-      id: 'cheese_3',
-      name: '치즈 3',
-      image: 'https://ssl.pstatic.net/static/nng/glive/icon/cheese03.png',
-    },
-    {
-      id: 'cheese_4',
-      name: '치즈 4',
-      image: 'https://ssl.pstatic.net/static/nng/glive/icon/cheese04.png',
-    },
-  ])
+app.get('/chzzk/badges', (req, res) => {
+  res.json(chzzkBadgeList);
+});
+
+app.get('/soop/badges', (req, res) => {
+  res.json(soopBadgeList);
 });
 
 ViteExpress.listen(app, 3033, () =>
---
+++ src/server/soop.ts
@@ -0,0 +1,24 @@
+const soopBadgeList = [
+    {
+        id: 'streamer',
+        name: '스트리머',
+        image: 'streamer',
+    },
+    {
+        id: 'manager',
+        name: '매니저',
+        image: 'manager',
+    },
+    {
+        id: 'vip',
+        name: '열혈팬',
+        image: 'vip',
+    },
+    {
+        id: 'fan',
+        name: '팬클럽',
+        image: 'fan',
+    },
+]
+
+export default soopBadgeList;
